d9fd7a0ad05a9a960d00079db543ee64f0d829c7
AlexeyMachehin <99137228+AlexeyMachehin@users.noreply.github.com>
2022-11-06 05:19:23 +0500

    Update productService.ts

--- src/redux/services/productService.ts
+++ src/redux/services/productService.ts
@@ -3,6 +3,7 @@ import axios from "../../axios";
 import { fromDto } from "../../mapper/product-mapper";
 
 const getProducts = async () => {
+  console.log(axios)
   const products = await axios.get<ProductDto[]>("/api/products"); // GET http://localhost:8000/api/products
   return products.data.map(product =>  fromDto(product));
 };
